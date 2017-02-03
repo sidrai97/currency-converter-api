ab4f748c18f5fe0c912456f3cc870bd8f4156739
sidrai97 <siddhantraisgr@gmail.com>
2017-02-03 20:30:18 +0530

    cors modified

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ def index_page():
 	print amount
 	print from_am
 	print to_am
+	bottle.response.headers['Access-Control-Allow-Origin'] = 'http://www.google.com/finance/converter'
 	return bottle.template('./views/api.tpl',a=amount,f=from_am,t=to_am)
 
 @bottle.error(404)
