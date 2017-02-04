9e55609c6ddc305376db6d2fbd3b0483a6da1ab7
sidrai97 <siddhantraisgr@gmail.com>
2017-02-04 13:12:27 +0530

    fix

--- app.py
+++ app.py
@@ -5,19 +5,22 @@ from BeautifulSoup import *
 
 @bottle.get('/api')
 def index_page():
-	amount=bottle.request.query.a
-	from_am=bottle.request.query.f
-	to_am=bottle.request.query.t
-	url='https://www.google.com/finance/converter?a='+amount+'&from='+from_am+'&to='+to_am
-	html_resp=urllib.urlopen(url).read()
-	soup = BeautifulSoup(html_resp)
-	json_resp=soup.find('div', id='currency_converter_result')
-	json_resp=map(str, json_resp.contents)
-	sp_str=json_resp[1]
-	sp_str=sp_str[18:sp_str.index('<',18)]
-	fp_str=json_resp[0].split(' ')
-	sp_str=sp_str.split(' ')
-	return '{"data":[{"from":["'+fp_str[1]+'","'+fp_str[0]+'"]},{"to":["'+sp_str[1]+'","'+sp_str[0]+'"]}]}'
+	try:
+		amount=bottle.request.query.a
+		from_am=bottle.request.query.f
+		to_am=bottle.request.query.t
+		url='https://www.google.com/finance/converter?a='+amount+'&from='+from_am+'&to='+to_am
+		html_resp=urllib.urlopen(url).read()
+		soup = BeautifulSoup(html_resp)
+		json_resp=soup.find('div', id='currency_converter_result')
+		json_resp=map(str, json_resp.contents)
+		sp_str=json_resp[1]
+		sp_str=sp_str[18:sp_str.index('<',18)]
+		fp_str=json_resp[0].split(' ')
+		sp_str=sp_str.split(' ')
+		return '{"data":[{"from":["'+fp_str[1]+'","'+fp_str[0]+'"]},{"to":["'+sp_str[1]+'","'+sp_str[0]+'"]}]}'
+	except:
+		return bottle.redirect('https://sidrai97.github.io/currency-converter-api/')	
 
 @bottle.route('/')
 @bottle.route('/documentation')
